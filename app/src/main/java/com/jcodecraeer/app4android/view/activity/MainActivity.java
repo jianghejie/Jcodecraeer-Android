91a8ee2e2d92656b9d44cc6818b5e9d23229cb61
weifucheng <786318384@qq.com>
2016-02-18 17:18:45 +0800

    MVP架构搭建

--- app/src/main/java/com/jcodecraeer/app4android/view/activity/MainActivity.java
+++ app/src/main/java/com/jcodecraeer/app4android/view/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.jcodecraeer.app4android;
+package com.jcodecraeer.app4android.view.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -6,8 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import com.jcodecraeer.app4android.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,25 +28,5 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
